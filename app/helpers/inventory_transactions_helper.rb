585b68aa5dcd26f9bff26757858a6f15ed0866f0
Brandon <the_crow_resurrection2005@hotmail.com>
2024-10-16 12:29:20 -0400

    fix table

--- app/helpers/inventory_transactions_helper.rb
+++ app/helpers/inventory_transactions_helper.rb
@@ -1,2 +1,12 @@
 module InventoryTransactionsHelper
+  def human_readable_transaction_type(transaction_type)
+    case transaction_type
+    when 'IN'
+      'Entrada'
+    when 'OUT'
+      'Salida'
+    else
+      transaction_type
+    end
+  end
 end
